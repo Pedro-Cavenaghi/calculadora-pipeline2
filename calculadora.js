a1279b79a570f9ffd15424ead9a7fe1f73fdac7d
Pedro-Cavenaghi <phenrycavenaghi164@gmail.com>
2026-08-29 12:38:01 -0300

    feat: setup inicial do projeto node.js com pipeline ci

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function subtrai(a, b) {
+  return a - b;
+}
+
+function multiplica(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Divisao por zero nao e permitida');
+  }
+  return a / b;
+}
+
+module.exports = { soma, subtrai, multiplica, divide };
---
+++ calculadora.test.js
@@ -0,0 +1,21 @@
+const { soma, subtrai, multiplica, divide } = require('./calculadora');
+
+test('deve somar dois numeros corretamente', () => {
+  expect(soma(2, 3)).toBe(5);
+});
+
+test('deve subtrair dois numeros corretamente', () => {
+  expect(subtrai(5, 2)).toBe(3);
+});
+
+test('deve multiplicar dois numeros corretamente', () => {
+  expect(multiplica(4, 3)).toBe(12);
+});
+
+test('deve dividir dois numeros corretamente', () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test('deve lancar erro ao tentar dividir por zero', () => {
+  expect(() => divide(10, 0)).toThrow('Divisao por zero nao e permitida');
+});
